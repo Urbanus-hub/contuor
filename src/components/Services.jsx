d3e89bdfd3c5a8e5459e9a13d8b97661a2a0f00a
urbanus-hub <urbankollyk@gmail.com>
2025-09-17 17:35:32 +0300

    refactor: Clean up Services and Shop components for improved readability and consistency

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,50 +1,78 @@
-import React,{useContext} from 'react'
-import { WhyUs } from './WhyUsCard'
-import { CraftContext } from '../App';
-import Testimonials from './Testimonials';
+import React, { useContext } from "react";
+import { WhyUs } from "./WhyUsCard";
+import { CraftContext } from "../App";
+import Testimonials from "./Testimonials";
 
 const Services = () => {
-  const data=useContext(CraftContext);
-  const whyUs=[...data,...useContext(CraftContext)];
-  
+  const { whyUs } = useContext(CraftContext);
+  const why = [...whyUs,...whyUs];
+
   return (
-    <div className='bg-[#EFF2F1]'>
-      <div className='w-full lg:h-[75vh] h-auto bg-[#3B5D50] px-5 sm:px-10 md:px-16 lg:px-25 flex lg:flex-row flex-col items-center lg:justify-between justify-center py-10 lg:py-0 mb-5'>
+    <div className="bg-[#EFF2F1]">
+      <div className="w-full lg:h-[75vh] h-auto bg-[#3B5D50] px-5 sm:px-10 md:px-16 lg:px-25 flex lg:flex-row flex-col items-center lg:justify-between justify-center py-10 lg:py-0 mb-5">
         {/* content */}
-        <div className='lg:w-[35%] w-full flex flex-col gap-5 text-center lg:text-left'>
-          <h1 className="text-white lg:text-5xl text-2xl sm:text-3xl md:text-4xl font-black mb-[30px] lg:mb-0 mt-10 lg:mt-0">Services</h1>
-          <p className='text-[#9DAEA2] mb-[24px] text-sm md:text-base'>Experience personalized interior solutions, expert consultations, and seamless delivery. Our team is dedicated to helping you bring your vision to life—every step of the way.
-</p>
-          <div className='flex flex-col sm:flex-row gap-5 justify-center lg:justify-start'>
-            <button className='px-8 py-3 rounded-full bg-[#F8B810] border-0 font-medium text-[17px] cursor-pointer'>Shop Now</button>
-            <button className='px-8 py-3 border-2 rounded-full border-white/50 cursor-pointer text-white'>Explore</button>
+        <div className="lg:w-[35%] w-full flex flex-col gap-5 text-center lg:text-left">
+          <h1 className="text-white lg:text-5xl text-2xl sm:text-3xl md:text-4xl font-black mb-[30px] lg:mb-0 mt-10 lg:mt-0">
+            Services
+          </h1>
+          <p className="text-[#9DAEA2] mb-[24px] text-sm md:text-base">
+            Experience personalized interior solutions, expert consultations,
+            and seamless delivery. Our team is dedicated to helping you bring
+            your vision to life—every step of the way.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-5 justify-center lg:justify-start">
+            <button className="px-8 py-3 rounded-full bg-[#F8B810] border-0 font-medium text-[17px] cursor-pointer">
+              Shop Now
+            </button>
+            <button className="px-8 py-3 border-2 rounded-full border-white/50 cursor-pointer text-white">
+              Explore
+            </button>
           </div>
         </div>
-        
+
         {/* hero img */}
-        <div className='lg:w-[55%] w-full h-[300px] md:h-[400px] lg:h-full flex-col items-end justify-end relative mt-10 lg:mt-0'>
-          <img src="images/dots-light.svg" alt="" className='absolute right-[-50px] lg:right-[-100px] top-10 lg:top-22 hidden md:block' />
-          <img src="images/couch.png" alt=""  className='absolute lg:top-16 top-0 right-[-10px] lg:right-[-10px] z-3 w-full h-full object-contain'/>
+        <div className="lg:w-[55%] w-full h-[300px] md:h-[400px] lg:h-full flex-col items-end justify-end relative mt-10 lg:mt-0">
+          <img
+            src="images/dots-light.svg"
+            alt=""
+            className="absolute right-[-50px] lg:right-[-100px] top-10 lg:top-22 hidden md:block"
+          />
+          <img
+            src="images/couch.png"
+            alt=""
+            className="absolute lg:top-16 top-0 right-[-10px] lg:right-[-10px] z-3 w-full h-full object-contain"
+          />
         </div>
       </div>
-      
-      <div className='w-full px-5 py-20 sm:px-10 md:px-16 lg:px-25 flex flex-col md:flex-row md:flex-wrap lg:flex-wrap gap-4 lg:gap-[30px] bg-[#EFF2F1]'>
-        {whyUs.map((item,index)=>(
-          <div key={index} className='w-full md:w-[45%] lg:w-[22%] h-auto md:h-[30vh] relative flex flex-col p-4 mb-6 md:mb-0'>
-            <div className='relative w-10 h-10 ml-4 mt-3 mb-5 bg-[#DCE5E4] rounded-full'>
-              <img src={item.image} alt=""  className='absolute left-[-20px] top-[-7px] w-10 h-10'/>
+
+      <div className="w-full px-5 py-20 sm:px-10 md:px-16 lg:px-25 flex flex-col md:flex-row md:flex-wrap lg:flex-wrap gap-4 lg:gap-[30px] bg-[#EFF2F1]">
+        {why.map((item, index) => (
+          <div
+            key={index}
+            className="w-full md:w-[45%] lg:w-[22%] h-auto md:h-[30vh] relative flex flex-col p-4 mb-6 md:mb-0"
+          >
+            <div className="relative w-10 h-10 ml-4 mt-3 mb-5 bg-[#DCE5E4] rounded-full">
+              <img
+                src={item.image}
+                alt=""
+                className="absolute left-[-20px] top-[-7px] w-10 h-10"
+              />
             </div>
-            
-            <div className='flex-col'>
-              <h3 className='text-[14px] md:text-base font-medium mb-[14px]'>{item.title}</h3> 
-              <p className='text-[14px] md:text-base text-black/70 pr-3'>{item.description}</p>
+
+            <div className="flex-col">
+              <h3 className="text-[14px] md:text-base font-medium mb-[14px]">
+                {item.title}
+              </h3>
+              <p className="text-[14px] md:text-base text-black/70 pr-3">
+                {item.description}
+              </p>
             </div>
           </div>
         ))}
       </div>
-      <Testimonials/>
+      <Testimonials />
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,34 +1,32 @@
-import React,{useState} from 'react'
-import { CraftCard } from './Home'
-import { CraftContext } from '../App';
-import {crafts} from '../utils/items'
+import React, { useState } from "react";
+import { CraftCard } from "./Home";
+import { CraftContext } from "../App";
+import { crafts } from "../utils/items";
 // import Cart from './Cart'
 const Shop = () => {
-  const {cartItems,setCartItems}=React.useContext(CraftContext)
-  
-  
+  const { cartItems, setCartItems } = React.useContext(CraftContext);
 
   return (
-    <div className='min-h-[100vh] w-full bg-[#EFF2F1] '>
-      <div className='h-[20vh] lg:h-[40vh] w-full bg-[#3B5D50] flex flex-col justify-center items-start'>
-         <h2 className='text-3xl lg:text-5xl  mb-6 text-white px-5 font-bold lg:px-25 md:px-18 sm:px-25'>
+    <div className="min-h-[100vh] w-full bg-[#EFF2F1] ">
+      <div className="h-[20vh] lg:h-[40vh] w-full bg-[#3B5D50] flex flex-col justify-center items-start">
+        <h2 className="text-3xl lg:text-5xl  mb-6 text-white px-5 font-bold lg:px-25 md:px-18 sm:px-25">
           Shop
-         </h2>
+        </h2>
       </div>
       {/* products */}
-      <div className=' h-full px-5 flex flex-wrap mt-10 lg:pb-25 md:px-18  lg:px-25 sm:px-25'>
-        {
-          crafts.map((craft,index)=>(
-              
-            <CraftCard key={index} image={craft.image} title={craft.title} price={craft.price} id={craft.id}/>
-          ))
-        }
-       
-       
+      <div className=" h-full px-5 flex flex-wrap mt-10 lg:pb-25 md:px-18  lg:px-25 sm:px-25">
+        {crafts.map((craft, index) => (
+          <CraftCard
+            key={index}
+            image={craft.image}
+            title={craft.title}
+            price={craft.price}
+            id={craft.id}
+          />
+        ))}
       </div>
-    
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
